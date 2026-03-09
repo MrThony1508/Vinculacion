164b83b118641017275e19273fc855989d537dde
stalin.jaime1591 <anthony.toapanta4746@utc.edu.ec>
2026-03-09 12:40:39 -0500

    Filtrado de Periodo academico en el mapa inicial

--- Aplicaciones/MapaInicial/views.py
+++ Aplicaciones/MapaInicial/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from Aplicaciones.Estudiantes.models import Estudiante
 
 def inicio2(request):
+    # Consultamos los estudiantes que tengan coordenadas
     estudiantes_qs = Estudiante.objects.filter(
         grupo__latitud__isnull=False,
         grupo__longitud__isnull=False
     ).values(
         'nombre', 'apellido', 'carrera', 'tipo_practica', 'semestre', 
-        'periodo_academico', # Nombre correcto según tu error
+        'periodo_academico', 
         'grupo__nombre', 'grupo__latitud', 'grupo__longitud',
         'docente__nombre', 'docente__apellido'
     )
+
+    # Obtenemos los periodos únicos para llenar el select del filtro
+    periodos = Estudiante.objects.values_list('periodo_academico', flat=True).distinct().order_by('-periodo_academico')
 
     estudiantes_lista = []
     for e in estudiantes_qs:
@@ -20,7 +24,7 @@ def inicio2(request):
             'carrera': e['carrera'],
             'tipo_practica': e['tipo_practica'],
             'semestre': e['semestre'],
-            'periodo': e['periodo_academico'], # Lo guardamos como 'periodo' para el JS
+            'periodo': e['periodo_academico'], 
             'grupo_nombre': e['grupo__nombre'],
             'lat': float(e['grupo__latitud']),
             'lng': float(e['grupo__longitud']),
@@ -28,5 +32,6 @@ def inicio2(request):
         })
 
     return render(request, 'inicio2.html', {
-        'estudiantes': estudiantes_lista
+        'estudiantes': estudiantes_lista,
+        'periodos': periodos
     })

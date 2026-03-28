17000fa626f4444109b1b82a111e569ff8e8a55c
stalin.jaime1591 <anthony.toapanta4746@utc.edu.ec>
2026-03-28 17:41:31 -0500

    Cambio de ingles a español en el area de adminsitrar un docente

--- Aplicaciones/Docentes/models.py
+++ Aplicaciones/Docentes/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Docente(models.Model):
-    # Cambiamos a ForeignKey para permitir múltiples registros por usuario
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     
-    # Quitamos unique=True porque el mismo docente aparecerá varias veces (una por periodo)
+    
     cedula = models.CharField(max_length=10) 
     correo_institucional = models.EmailField()
     
@@ -18,7 +18,7 @@ class Docente(models.Model):
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.periodo}"
 class Programa(models.Model):
-    # Asegúrate de que se llamen así:
+    
     proyecto = models.CharField(max_length=255) 
     coordinador = models.CharField(max_length=255)
     periodo = models.CharField(max_length=100)
